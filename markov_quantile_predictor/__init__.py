e944a677ed31efe28aec1ee64bbf82cd885ccbbb
avrich5 <achyrva@mmi.ai>
2025-03-22 12:01:33 +0100

    Исправлен путь импорта в __init__.py

--- markov_quantile_predictor/__init__.py
+++ markov_quantile_predictor/__init__.py
@@ -3,9 +3,9 @@ Markov Quantile Predictor - гибридный предиктор временн
 объединяющий марковские цепи и квантильную регрессию.
 """
 
+from .config import PredictorConfig
 from .markov_predictor import MarkovPredictor
 from .quantile_regression import QuantileRegressionModel
 from .hybrid_predictor import MarkovQuantilePredictor
-from .markov_quantile_predictor.config import PredictorConfig
 
 __version__ = '0.1.0'
